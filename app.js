dae3376398d9c20170dedc6fb1b79883298ce890
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2021-05-11 11:59:04 +0100

    Initial logic skeleton

---
+++ app.js
@@ -0,0 +1,20 @@
+const game = (function () {
+  //
+})();
+
+const gameBoard = (function () {
+  let board = [null, null, null, null, null, null, null, null, null];
+})();
+
+const Player = (name, mark) => {
+  const state = {
+    name,
+    mark,
+  };
+
+  return {};
+};
+
+const displayController = (function () {
+  //
+})();
